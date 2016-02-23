4f2fc78285e1cca0b99e327addbc4522a2c38b89
msherman1229 <msherman1229@gmail.com>
2016-02-23 00:42:35 +0000

    Funcitonal

--- script16.js
+++ script16.js
@@ -2,9 +2,9 @@ var app = angular.module("app", []);
 app.controller("mainCtrl", mainCtrl); 
 
 function mainCtrl ($scope) {
-	$scope.logs = []; 
+	$scope.logs = [];
+	var randID = Math.floor((Math.random() * 10000000) + 1); 
 	$scope.submitNew = function (newText) {
-		//$scope.logs = [];
 		$scope.logs.push({
 			userText: newText.submission, 
 			type: 0 
@@ -13,7 +13,7 @@ function mainCtrl ($scope) {
 		$("#yourText").val(''); 
 		//might want to generate a unique id each time, so that we aren't all the same speaker
 		//have them submit name and age or something and then I can use that as the key  
-		var urlCall = 'http://www.personalityforge.com/api/chat/?apiKey=OGolx9hVeyDkEkW5&chatBotID=2&message=' + newText.submission + '&externalID=abc-639184572'; 
+		var urlCall = 'http://www.personalityforge.com/api/chat/?apiKey=OGolx9hVeyDkEkW5&chatBotID=2&message=' + newText.submission + '&externalID=' + randID; 
 		var api_call = $.ajax({
 			url: urlCall,  
 			type: 'GET', 
@@ -43,6 +43,6 @@ function mainCtrl ($scope) {
 		api_call.fail(function(jqXHR, textStatus) {
 			console.log(jqXHR);
 			console.log(textStatus);
-		});  
-	};
+		});
+	}
 }
